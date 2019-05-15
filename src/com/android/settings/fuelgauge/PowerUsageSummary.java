da1e4d08d2fa559a31cfae07d99b8d88a2d0afab
Anushek Prasal <anushekprasal@gmail.com>
2019-05-15 14:13:33 +0530

    Settings: Add toggle to disable battery estimates in QS [2/2]
    -Restart SystemUI when toggling battery estimates
    
    Change-Id: Iaac8cce1fa0b7a1b562a1a4b6e2459190a765716
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -47,6 +47,7 @@ import com.android.settings.Utils;
 import com.android.settings.applications.LayoutPreference;
 import com.android.settings.core.SubSettingLauncher;
 import com.android.settings.dashboard.SummaryLoader;
+import com.android.settings.fuelgauge.BatteryEstimatesPreferenceController;
 import com.android.settings.fuelgauge.UpTimePreferenceController;
 import com.android.settings.fuelgauge.anomaly.Anomaly;
 import com.android.settings.fuelgauge.anomaly.AnomalyDetectionPolicy;
@@ -277,6 +278,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 BatteryTipListener */);
         controllers.add(mBatteryTipPreferenceController);
         controllers.add(new SmartPixelsPreferenceController(context));
+        controllers.add(new BatteryEstimatesPreferenceController(context));
         controllers.add(new UpTimePreferenceController(context));
         return controllers;
     }
